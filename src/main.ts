5ee6766d62298037a4af09862da896dfcf500409
Viktor Patraboi <viktor.patraboi@medhost.com>
2022-01-18 05:19:29 -0600

    updated port

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
